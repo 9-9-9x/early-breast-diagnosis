2aeaccdb1fdef0261ceb0333c74267cd84536de4
Raihan <17radf@gmail.com>
2025-10-12 22:32:34 +0700

    feat: real data for settings

--- resources/views/pages/pengaturan/index.blade.php
+++ resources/views/pages/pengaturan/index.blade.php
@@ -20,19 +20,8 @@
             {{-- Profile Picture Section --}}
             <div class="flex justify-center">
                 <div class="relative">
-                    {{-- The label makes the image clickable for file upload --}}
-                    <label for="profile_photo" class="cursor-pointer">
-                        <img src="https://via.placeholder.com/125" alt="Profile User" class="w-32 h-32 rounded-full object-cover border-2 border-gray-200">
-                    </label>
-                    {{-- Hidden file input --}}
-                    <input type="file" name="profile_photo" id="profile_photo" class="hidden">
+                    <img src="{{ asset('profile.png') }}" alt="Logo Puskesmas" class="w-30 h-30 object-cover ">
 
-                    {{-- Edit Icon Button --}}
-                    <label for="profile_photo" class="absolute bottom-0 right-0 cursor-pointer">
-                        <div class="bg-white rounded-full p-2 shadow-md border border-gray-200">
-                             <svg class="w-6 h-6 text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-                        </div>
-                    </label>
                 </div>
             </div>
 
@@ -45,22 +34,17 @@
 
                 <div>
                     <label for="nama" class="{{ $labelClasses }}">Nama</label>
-                    <input type="text" id="nama" name="nama" value="Ranee Alleyda" class="{{ $inputClasses }}">
-                </div>
-
-                <div>
-                    <label for="tanggal_lahir" class="{{ $labelClasses }}">Tanggal Lahir</label>
-                    <input type="text" id="tanggal_lahir" name="tanggal_lahir" placeholder="DD/MM/YYYY" class="{{ $inputClasses }}">
+                    <input type="text" id="nama" name="nama" value="{{ auth()->user()->name }}" class="{{ $inputClasses }}">
                 </div>
 
                 <div>
                     <label for="telepon" class="{{ $labelClasses }}">Nomor Telepon</label>
-                    <input type="tel" id="telepon" name="telepon" class="{{ $inputClasses }}">
+                    <input type="tel" id="telepon" name="telepon" value="{{ auth()->user()->patientProfile()->nomor_telepon ?? '' }}" class="{{ $inputClasses }}">
                 </div>
 
                 <div>
                     <label for="email" class="{{ $labelClasses }}">Email</label>
-                    <input type="email" id="email" name="email" class="{{ $inputClasses }}">
+                    <input type="email" id="email" name="email" value="{{ auth()->user()->email ?? '' }}" class="{{ $inputClasses }}">
                 </div>
             </div>
         </div>
